6f8abcc6aea72ee8ff452d871aa72a4aa7907ddf
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2026-05-20 16:47:24 -0400

    Add CORS middleware and new route handlers
    Added CORS middleware and new routers for costs, dashboards, production, reports, and plots. Removed unused routers for inputs and agricultural activities.

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from controllers.auth_routes import router_auth
 from controllers.fazendas import router_fazendas
 from controllers.maquinas import router_maquinas
 from controllers.operadores import router_operadores
 from controllers.culturas import router_culturas
-from controllers.insumos import router_defensivos
-from controllers.insumos import router_sementes
-from controllers.insumos import router_fertilizantes
-from controllers.atividadesagricolas import router_atividades
+from controllers.custos import router_custos
+from controllers.dashboards import router_dashboard
+from controllers.producao import router_producao
+from controllers.relatorios import router_relatorios
+from controllers.talhoes import router_talhoes
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(router_auth)
 app.include_router(router_fazendas)
 app.include_router(router_maquinas)
 app.include_router(router_operadores)
 app.include_router(router_culturas)
-app.include_router(router_fertilizantes)
-app.include_router(router_defensivos)
-app.include_router(router_sementes)
-app.include_router(router_atividades)
-app.include_router(router_auth)
+app.include_router(router_custos)
+app.include_router(router_dashboard)
+app.include_router(router_producao)
+app.include_router(router_relatorios)
+app.include_router(router_talhoes)
 
+
+@app.get('/')
+def home():
+    return {
+        'mensagem': 'API funcionando'
+    }
