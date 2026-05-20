e7840d209b8d149d262fd57b79be9077f2a307ae
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2026-05-20 16:44:46 -0400

    Implement listar_maquinas endpoint in maquinas router
    Replaced placeholder functions with a working endpoint that returns a message.

--- controllers/maquinas.py
+++ controllers/maquinas.py
@@ -1,32 +1,13 @@
 from fastapi import APIRouter
 
-router_maquinas = APIRouter(prefix="/maquinas")
-
-@router_maquinas.get('/')  
-
-def consultar_maquinas():
-    pass
+router_maquinas = APIRouter(
+    prefix='/maquinas',
+    tags=['Máquinas']
+)
 
 
-@router_maquinas.get('/{id}')  
-
-def consultar_maquina(id: int):
-    pass
-
-
-@router_maquinas.post('/')  
-
-def cadastrar_maquinas():
-    pass
-
-
-@router_maquinas.put('/{id}')  
-
-def alterar_maquina(id: int):
-    pass
-
-
-@router_maquinas.delete('/{id}')  
-
-def deletar_maquina(id: int):
-    pass
+@router_maquinas.get('/')
+def listar_maquinas():
+    return {
+        'mensagem': 'Rota máquinas funcionando'
+    }
