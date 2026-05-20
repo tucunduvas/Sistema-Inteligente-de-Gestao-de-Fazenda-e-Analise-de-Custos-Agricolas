795d8351bdf7ab9bb1160591318600d5db8c8631
Selthon André Gomes leal <163306797+selthon06@users.noreply.github.com>
2026-05-20 16:45:32 -0400

    Implement listing endpoint for operadores
    Replaced placeholder functions with a working endpoint to list operators.

--- controllers/operadores.py
+++ controllers/operadores.py
@@ -1,31 +1,13 @@
 from fastapi import APIRouter
 
-router_operadores = APIRouter(prefix="operadores")
-
-@router_operadores.get('/')  
-
-def consultar_operadores():
-    pass
+router_operadores = APIRouter(
+    prefix='/operadores',
+    tags=['Operadores']
+)
 
 
-@router_operadores.get('/{id}')  
-
-def consultar_operador(id: int):
-    pass
-
-@router_operadores.post('/')  
-
-def cadastrar_operadores():
-    pass
-
-
-@router_operadores.put('/{id}')  
-
-def alterar_operador(id: int):
-    pass
-
-
-@router_operadores.delete('/{id}')  
-
-def deletar_operador(id: int):
-    pass
+@router_operadores.get('/')
+def listar_operadores():
+    return {
+        'mensagem': 'Rota operadores funcionando'
+    }
